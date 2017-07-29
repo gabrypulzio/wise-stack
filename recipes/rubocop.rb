cd79b49c38d9d424f0186579a9ef3b70ea17e799
Gabriele Pulzato <gabriele@webleganza.com>
2017-07-29 10:14:34 +0200

    Give priority to template rubocop.yml over user default configuration file. Use a bundled rubocop.yml
    Since user default configuration file may not handle rails specific configuration, wise-stack new
    defaults is asking to install template configuration file.

--- recipes/rubocop.rb
+++ recipes/rubocop.rb
@@ -3,11 +3,12 @@
 gem_install 'rubocop'
 
 after_bundle do
+  copy_template_rubocop = true
   if File.exist? File.expand_path('~/.rubocop.yml')
-    say_info 'using user .rubocop.yml file located at ~/.rubocop.yml'
-  else
-    say_info 'downloading .rubocop.yml'
-    get 'https://gist.githubusercontent.com/gabrypulzio/892dbb0bae11048da89772fcb109705c/raw/5a39c628d3e274140a973aa4d0af7baa0ab65da8/.rubocop.yml'
+    unless yep? 'Install template rubocop.yml file? (if no, the user ~/.rubocop.yml will be used)'
+      say_info 'Using user .rubocop.yml file located at ~/.rubocop.yml'
+      copy_template_rubocop = false
+    end
   end
-  run 'rubocop -a --fail-level A > /dev/null'
+  copy_file 'templates/.rubocop.yml', '.rubocop.yml' if copy_template_rubocop
 end
