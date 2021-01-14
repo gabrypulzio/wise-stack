7f901f6243e255f47f46206bef69c47c753376b7
Gabriele Pulzato <gabriele@webleganza.com>
2021-01-14 22:52:48 +0100

    Fix yarn_package_installed?

--- lib/yarn.rb
+++ lib/yarn.rb
@@ -2,9 +2,9 @@
 
 require 'json'
 
-def yarn_package_installed?(_package)
+def yarn_package_installed?(package)
   return false unless File.exist?('package.json')
 
   json = JSON.parse(File.read('package.json'))
-  json['dependencies'] && json['dependencies']['bootstrap'].present?
+  json['dependencies'] && json['dependencies'][package].present?
 end
